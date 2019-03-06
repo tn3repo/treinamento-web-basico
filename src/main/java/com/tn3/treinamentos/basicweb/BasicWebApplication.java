7dbc6c20bd1e46d967dcde23be23fd640c3d8a28
fabio.f <fabio.f@tn3.com.br>
2019-03-06 09:29:48 -0300

    Commit inicial com estrutura do projeto

---
+++ src/main/java/com/tn3/treinamentos/basicweb/BasicWebApplication.java
@@ -0,0 +1,13 @@
+package com.tn3.treinamentos.basicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tn3/treinamentos/basicweb/BasicWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tn3.treinamentos.basicweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
